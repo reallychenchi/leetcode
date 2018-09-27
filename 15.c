d9fd936a86aab4394b8c14c0855307e5455050f3
cook <cookchen@tcl.com>
2018-09-27 18:20:17 +0800

    item 15
    Signed-off-by: cook <cookchen@tcl.com>

--- 15.c
+++ 15.c
@@ -45,38 +45,96 @@ void sort(int *nums, int s, int e){
     sort(nums, idx + 1, e);
 }
 
-int search(int *nums, int s, int e, int k){
+int search(int *nums, int s, int e, int k, bool findNear){
+    if (s == e){
+        if (nums[e] == k) return e;
+        else {
+            if (findNear) return e;
+            else return -1;
+        }
+    }
     if (s >= e){
-        return -1;
+        if (findNear){
+            return nums[e] > 0 ? s:e;
+        }else{
+            return -1;
+        }
     }
     int m = (s + e) / 2;
     if (k > nums[m]){
-        return search(nums, s, (s + e) / 2 - 1, k);
+        return search(nums, s, m - 1, k, findNear);
     }else if (k == nums[m]){
         return m;
     }else{
-        return search(nums, (s + e) / 2 + 1, e, k);
+        return search(nums, m + 1, e, k, findNear);
     }
+}
+
+bool isExists(int *r, int** ret, int sz){
+    for(int i = 0; i < sz; ++i){
+        int *data = ret[i];
+        int j = 0;
+        for(; j < 3; ++j){
+            if (r[j] != data[j]){
+                break;
+            }
+        }
+        if (j == 3) return true;
+    }
+
+    return false;
+}
+
+int** threeSum1(int* nums, int numsSize, int* returnSize) {
+    int ** ret = malloc(numsSize * numsSize * sizeof(int *));
+    *returnSize = 0;
+    if (numsSize < 3) return ret;
+
+    int *r = malloc(3 * sizeof(int));
+    for(int i = 0; i < numsSize - 2; ++i){
+        r[0] = nums[i];
+        for(int j = i + 1; j < numsSize - 1; ++j){
+            r[1] = nums[j];
+            r[2] = 0 - r[0] - r[1];
+            for(int k = j + 1; k < numsSize; ++k){
+                sort(r, 0, 3);
+                if (r[2] == nums[k] && !isExists(r, ret, *returnSize)){
+                    ret[*returnSize] = r;
+                    *returnSize = *returnSize + 1;
+                    r = malloc(3 * sizeof(int));
+                    r[0] = nums[i];
+                    break;
+                }else{
+                    r[0] = nums[i];
+                    r[1] = nums[j];
+                    r[2] = 0 - r[0] - r[1];
+                }
+            }
+        }
+    }
+    return ret;
 }
 
 int** threeSum(int* nums, int numsSize, int* returnSize) {
     int ** ret = malloc(numsSize * numsSize * sizeof(int *));
     *returnSize = 0;
+    if (numsSize < 3) return ret;
+
     sort(nums, 0, numsSize);    
-    for(int i = 0; i < numsSize / 2; ++i){
-        int r1 = nums[i];
-        int r2 = 0;
-        int r3 = 0;
-        for(int j = numsSize - 1; j >= numsSize / 2; --j){
-            r2 = nums[j];
-            r3 = 0 - r2 - r1;
-            int r3Idx = search(nums, i + 1, j, r3);
-            if (r3Idx != -1){
-                ret[*returnSize] = malloc(3 * sizeof(int));
-                ret[*returnSize][0] = r1;
-                ret[*returnSize][1] = r2;
-                ret[*returnSize][2] = r3;
+    int zeroIdx = search(nums, 0, numsSize, 0, true);
+    if (zeroIdx < 0 || zeroIdx >= numsSize) return ret;
+    int *r = malloc(3 * sizeof(int));
+    for(int i = 0; i < zeroIdx; ++i){
+        r[0] = nums[i];
+        for(int j = numsSize - 1; j >= zeroIdx; --j){
+            r[2] = nums[j];
+            r[1] = 0 - r[2] - r[0];
+            int r3Idx = search(nums, i + 1, j, r[1], false);
+            if (r3Idx != -1 && !isExists(r, ret, *returnSize)){
+                ret[*returnSize] = r;
                 *returnSize = *returnSize + 1;
+                r = malloc(3 * sizeof(int));
+                r[0] = nums[i];
             }
         }
     }
@@ -84,12 +142,35 @@ int** threeSum(int* nums, int numsSize, int* returnSize) {
 }
 
 void checkResult(int * a, int sz){
-    int rsz;
-    int ** r = threeSum(a, sz, &rsz);
-    PT("CHECK\n");
-    for(int i = 0; i < rsz; ++i){
-        PT("Result %d, %d, %d\n", r[i][0], r[i][1], r[i][2]);
+    PT("\nCHECK:\t");
+    for(int i = 0; i < sz; ++i){
+        PT("%d, ", a[i]);
     }
+    PT("\n");
+
+    int rsz1;
+    int ** r1 = threeSum1(a, sz, &rsz1);
+    int rsz = 0;
+    int ** r = NULL;//threeSum(a, sz, &rsz);
+    if (rsz1 != rsz){
+        PT("FAILED: SZ %d, %d\n", rsz, rsz1);
+        return;
+    }
+    for(int i = 0; i < rsz; ++i){
+        if (!isExists(r[i], r1, rsz)){
+            PT("FAILED: WRONG EXISTS %d, %d, %d\n", r[i][0], r[i][1], r[i][2]);
+            return;
+        }
+    }
+
+    for(int i = 0; i < rsz; ++i){
+        if (!isExists(r1[i], r, rsz)){
+            PT("FAILED: NOT FOUND %d, %d, %d\n", r1[i][0], r1[i][1], r1[i][2]);
+            return;
+        }
+    }
+
+    PT("PASS\n");
 }
 
 int main(){
@@ -99,5 +180,23 @@ int main(){
 	checkResult(b, 16);
     int c[] = {-1, 0, 1, 2, -1, -4};
 	checkResult(c, 6);
+    int d[] = {0, 0, 0};
+	checkResult(d, 3);
+    int e[] = {1, 0, -1};
+	checkResult(e, 3);
+    int f[] = {1,2,-2,-1};
+	checkResult(f, 4);
+    int g[] = {0, 0};
+	checkResult(g, 2);
+    int h[] = {1, 1, 1};
+	checkResult(h, 3);
+    int i[] = {1, 1, -2};
+	checkResult(i, 3);
+    int j[] = {-2,0,1,1,2};
+	checkResult(j, 5);
+    int k[] = {3,0,3,2,-4,0,-3,2,2,0,-1,-5};
+	checkResult(k, 12);
+    int l[] = {-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0};
+	checkResult(l, 16);
     return 0;
 }
