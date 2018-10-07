d088976e2cd48d8ea4a9ba5dd28d239b0ef192f6
Jincao Chen <me@chenchi.cc>
2018-10-07 16:08:34 +0800

    Item 15 pass
    Signed-off-by: Jincao Chen <me@chenchi.cc>

--- 15.c
+++ 15.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#define PT printf
 #define true 1
 #define false 0
 typedef int bool;
+#define PT printf
 
 //https://leetcode.com/problems/3sum/
 void swap(int *l, int *r){
@@ -47,13 +47,10 @@ void sort(int *nums, int s, int e){
 
 int search(int *nums, int s, int e, int k, bool findNear){
     if (s == e){
-        if (nums[e] == k) return e;
-        else {
             if (findNear) return e;
             else return -1;
-        }
     }
-    if (s >= e){
+    if (s > e){
         if (findNear){
             return nums[e] > 0 ? s:e;
         }else{
@@ -62,7 +59,7 @@ int search(int *nums, int s, int e, int k, bool findNear){
     }
     int m = (s + e) / 2;
     if (k > nums[m]){
-        return search(nums, s, m - 1, k, findNear);
+        return search(nums, s, m, k, findNear);
     }else if (k == nums[m]){
         return m;
     }else{
@@ -71,6 +68,7 @@ int search(int *nums, int s, int e, int k, bool findNear){
 }
 
 bool isExists(int *r, int** ret, int sz){
+    sort(r, 0, 3);
     for(int i = 0; i < sz; ++i){
         int *data = ret[i];
         int j = 0;
@@ -97,7 +95,6 @@ int** threeSum1(int* nums, int numsSize, int* returnSize) {
             r[1] = nums[j];
             r[2] = 0 - r[0] - r[1];
             for(int k = j + 1; k < numsSize; ++k){
-                sort(r, 0, 3);
                 if (r[2] == nums[k] && !isExists(r, ret, *returnSize)){
                     ret[*returnSize] = r;
                     *returnSize = *returnSize + 1;
@@ -123,10 +120,24 @@ int** threeSum(int* nums, int numsSize, int* returnSize) {
     sort(nums, 0, numsSize);    
     int zeroIdx = search(nums, 0, numsSize, 0, true);
     if (zeroIdx < 0 || zeroIdx >= numsSize) return ret;
+    int zeroIdxI = zeroIdx;
+    while(nums[zeroIdxI] == 0 && zeroIdxI > 0){zeroIdxI--;}
+    zeroIdxI++;
+    int zeroIdxJ = zeroIdx;
+    while(nums[zeroIdxJ] == 0 && zeroIdxJ < numsSize){zeroIdxJ++;}
+    zeroIdxJ--;
+    
     int *r = malloc(3 * sizeof(int));
-    for(int i = 0; i < zeroIdx; ++i){
+
+    if (zeroIdxJ - zeroIdxI >= 2){
+        memset(r, 0, 3 * sizeof(int));
+        ret[*returnSize] = r;
+        *returnSize = *returnSize + 1;
+        r = malloc(3 * sizeof(int));
+    }
+    for(int i = 0; i < zeroIdxI; ++i){
         r[0] = nums[i];
-        for(int j = numsSize - 1; j >= zeroIdx; --j){
+        for(int j = numsSize - 1; j >= zeroIdxJ; --j){
             r[2] = nums[j];
             r[1] = 0 - r[2] - r[0];
             int r3Idx = search(nums, i + 1, j, r[1], false);
@@ -151,26 +162,24 @@ void checkResult(int * a, int sz){
     int rsz1;
     int ** r1 = threeSum1(a, sz, &rsz1);
     int rsz = 0;
-    int ** r = NULL;//threeSum(a, sz, &rsz);
-    if (rsz1 != rsz){
-        PT("FAILED: SZ %d, %d\n", rsz, rsz1);
-        return;
-    }
+    int ** r = threeSum(a, sz, &rsz);
+    bool isPass = true;
     for(int i = 0; i < rsz; ++i){
-        if (!isExists(r[i], r1, rsz)){
+        if (!isExists(r[i], r1, rsz1)){
             PT("FAILED: WRONG EXISTS %d, %d, %d\n", r[i][0], r[i][1], r[i][2]);
-            return;
+            isPass = false;
         }
     }
 
-    for(int i = 0; i < rsz; ++i){
+    for(int i = 0; i < rsz1; ++i){
         if (!isExists(r1[i], r, rsz)){
             PT("FAILED: NOT FOUND %d, %d, %d\n", r1[i][0], r1[i][1], r1[i][2]);
-            return;
+            isPass = false;
         }
     }
-
-    PT("PASS\n");
+    if (isPass){
+        PT("PASS\n");
+    }
 }
 
 int main(){
@@ -198,5 +207,7 @@ int main(){
 	checkResult(k, 12);
     int l[] = {-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0};
 	checkResult(l, 16);
+    int m[] = {-1,4,0,0,-4,-5,-4,-5,0,2,-5,2,-2};
+	checkResult(m, 13);
     return 0;
 }
